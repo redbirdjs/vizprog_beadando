6c58170ed586549b6e7b236ea2ec9e90413d55ad
keitajs <dominikegersdorfer@gmail.com>
2026-04-24 12:38:03 +0200

    ModositBerles form hozzáadva

---
+++ ModositBerles.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Autoberles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace vizprog_beadando
+{
+    /// <summary>
+    /// Interaction logic for ModositBerles.xaml
+    /// </summary>
+    public partial class ModositBerles : Window
+    {
+        public ModositBerles(Berles berles)
+        {
+            InitializeComponent();
+        }
+    }
+}

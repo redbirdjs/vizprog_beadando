6898256240ec12e857edde896daf8476264fdc68
Gergő Mátrai <35497481+redbirdjs@users.noreply.github.com>
2026-04-24 09:10:24 +0200

    autók csv és automatikus adatfeltöltés

--- db/Database.cs
+++ db/Database.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Autoberles;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Windows;
-using Autoberles;
 
 namespace vizprog_beadando.db
 {
@@ -25,7 +27,31 @@ namespace vizprog_beadando.db
 
         private void FillWithData()
         {
-            
+            int db = 0;
+            try
+            {
+                StreamReader r = new StreamReader("db/autok.csv");
+                while (!r.EndOfStream)
+                {
+                    string[] data = r.ReadLine()!.Split(',');
+                    Auto row = new Auto
+                    {
+                        marka = data[0],
+                        tipus = data[1],
+                        berles_dij = Convert.ToInt32(data[2])
+                    };
+
+                    cn.Autok.Add(row);
+                    db++;
+                }
+
+                MessageBox.Show($"{db} példa adat feltöltése sikeresen megtörtént!", "Automatikus adatfeltöltés");
+                r.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Hiba történt a fájlok betöltése közben!\n{ex.Message}");
+            }
         }
     }
 }

b6c9201b88d424c282159fe12ce9793c38a1705c
Gergő Mátrai <35497481+redbirdjs@users.noreply.github.com>
2026-04-25 07:58:20 +0200

    autó és bérlés keresés ablakok létrehozva

---
+++ AutoKereso.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace vizprog_beadando
+{
+    /// <summary>
+    /// Interaction logic for AutoKereso.xaml
+    /// </summary>
+    public partial class AutoKereso : Window
+    {
+        public AutoKereso()
+        {
+            InitializeComponent();
+        }
+
+        private void autoKereso_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ BerloKereso.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace vizprog_beadando
+{
+    /// <summary>
+    /// Interaction logic for BerloKereso.xaml
+    /// </summary>
+    public partial class BerloKereso : Window
+    {
+        public BerloKereso()
+        {
+            InitializeComponent();
+        }
+
+        private void berloKereses_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
